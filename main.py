5cf2a2d399ae5fe3583ff0ba1c466dd9f77e6f57
frank-schotanus <115595498+frank-schotanus@users.noreply.github.com>
2023-10-17 12:42:17 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,32 @@
 import cv2
-import numpy as np 
-from matplotlib import pyplot as plt
+import numpy as np
+
+filename = 'square.jpg'
 
 #read in images
-og_img = cv2.imread('square.jpg', cv2.IMREAD_COLOR)
-og_template = cv2.imread('holetemplate.jpg', cv2.IMREAD_COLOR)
+og_img = cv2.imread(filename, cv2.IMREAD_COLOR)
 
 #convert to black and white
 bw_img = cv2.cvtColor(og_img, cv2.COLOR_BGR2GRAY)
-bw_template = cv2.cvtColor(og_template, cv2.COLOR_BGR2GRAY)
+
+"""
+#changing contrast & brightness
+#currently doesn't work for subsquare.jpg(??)
+alpha = 2.5  # Increase contrast(>1)
+beta = 0    #brightness(<0 dim, >0 bright)
+cntrst_img = cv2.convertScaleAbs(bw_img, alpha=alpha, beta=beta)
+"""
 
 #apply blur
 img = cv2.GaussianBlur(bw_img, (13, 13), 2)
-template = cv2.GaussianBlur(bw_template, (9,9), 2)
 
 #establish image dimensions so measurements can be made in angstroms instead of pixels
 image_width_pixels = img.shape[1]
-image_width_units = 745881.6
-#image_width_units = 107315.2 #automate this
+
+if (filename == 'subsquare.jpg'):
+    image_width_units = 107315.2
+elif(filename == 'square.jpg'):
+    image_width_units = 745881.6
 units_per_pixel = image_width_units / image_width_pixels
 angstrom_max_radius = 15000
 angstrom_min_dist = 10926
@@ -33,13 +42,13 @@ circles = cv2.HoughCircles(
     maxRadius = int(angstrom_max_radius / units_per_pixel )   # Maximum radius
 )
 
+#coordinates of circels
 circles = np.round(circles[0, :]).astype("int")
 
+#drawing circles on img
 for (x, y, r) in circles:
     cv2.circle(img, (x, y), r, (0, 255, 0), 2)
 
 cv2.imshow("Detected Circles", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-#print(cv2.getBuildInformation())
