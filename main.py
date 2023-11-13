b19dc9f7e5dff47d386df6664e530c2f71c8ca69
frank-schotanus <115595498+frank-schotanus@users.noreply.github.com>
2023-11-12 19:21:21 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import cv2
 import numpy as np
+import math
 
-filename = 'square.jpg'
+filename = 'subsquare.jpg'
+
 
 #read in images
 img = cv2.imread(filename, cv2.IMREAD_COLOR)
@@ -13,42 +15,65 @@ bw_template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
 if(filename=='square.jpg'):
     dsize = (40, 40)
     bw_template = cv2.resize(bw_template,dsize)
+elif(filename=='subsquare.jpg'):
+    dsize = (240, 240)
+    bw_template = cv2.resize(bw_template, dsize)
 w, h = bw_template.shape[::-1]
 
 
 res = cv2.matchTemplate(bw_img, bw_template, cv2.TM_CCOEFF_NORMED)
-threshold = .75
-loc = np.column_stack(np.where(res >= threshold))
-scores = [res[i[0], i[1]] for i in loc]
+threshold = .5
+loc = np.where(res >= threshold)
+scores = [res[x, y] for x, y in zip(*loc)]
 
+loc2 = (loc[1], loc[0])  #reverse x and y, (y, x) -> (x, y)
+points = loc2 + (scores,) #add corresponding score to each coordinate
 
 selected_points = []
+selected_scores = []
 circle = []
 circle_scores = []
 radius = h//2
-for (x, y), score in zip(loc, scores):
-    point = (x, y)
+for pt1 in zip(*points):
+    point = (pt1[0] + w // 2, pt1[1] + h // 2)
     circle.append(point)
-    circle_scores.append(score)
+    circle_scores.append(pt1[2])
     for pt in circle:
-        if np.linalg.norm(np.array(point) - np.array(pt)) > radius:
+        hypot = np.array(point) - np.array(pt)
+        if math.sqrt(pow(hypot[0], 2) + pow(hypot[1], 2)) > radius:
             circle.pop()
             circle_scores.pop()
             max_index = circle_scores.index(max(circle_scores))
+            selected_scores.append(max(circle_scores))
             selected_points.append(circle[max_index])
-            cv2.circle(img, circle[max_index], 1, (0,0,255),1)
             circle = []
             circle_scores = []
             break
 
-"""
+final_points = []
+final_scores = []
+for spt in selected_points:
+    final_points.append(spt)
+    final_scores.append(selected_scores[selected_points.index(spt)])
+    for fpt in final_points:
+        hypot = np.array(spt) - np.array(fpt)
+        if math.sqrt(pow(hypot[0], 2) + pow(hypot[1], 2)) < radius: # combine
+            if(final_scores[final_points.index(fpt)] < final_scores[final_points.index(spt)]):
+                index = final_points.index(fpt)
+                final_points.pop(index)
+                final_scores.pop(index)
+            elif(final_scores[final_points.index(fpt)] > final_scores[final_points.index(spt)]):
+                index = final_points.index(spt)
+                final_points.pop(index)
+                final_scores.pop(index)
+
 #drawing circles and '+'
-for pt in selected_points:
+for pt in final_points:
     center = (pt[0], pt[1])
+    confidence = (1 - final_scores[final_points.index(pt)]) * (1 / (1-threshold)) * 100#lower number = better score/confidence
     cv2.circle(img, center, h//4, (0, 0, 255), 1)
     cv2.line(img, (center[0] - h//5, center[1]), (center[0] + h//5, center[1]), (0, 0, 0), 1)
     cv2.line(img, (center[0], center[1] - h//5), (center[0], center[1] + h//5), (0, 0, 0), 1)
-"""
 
 #showing image
 cv2.imshow("Detected Circles", img)
